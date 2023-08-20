5cc497e2b4973f1f826cd283ec8f4ced575f5dba
Santiago Acevedo Arango <isantiago20@gmail.com>
2023-08-19 23:45:45 -0500

    Grammar Checked and Food redefined to Nutrition

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # mixedMessages
 
-The objective of this project is to make an application with an IDE and Git Version Control, that generates random messages with tips to have healthy lifestyle. It can include general information relevant to someone who wants to improve his health, and also specific advices to fulfill different goals, like losing weigth, aconditioning, gaining muscle mass, and more.
+The objective of this project is to make an application with an IDE and Git Version Control, that generates random messages with tips to have healthy lifestyle. It can include general information relevant to someone who wants to improve his health, and also specific advices to fulfill different goals, like losing weight, conditioning, gaining muscle mass, and more.
 
 It will include at least 15 different messages, randomized and no longer than 250 characters.
 
@@ -10,25 +10,25 @@ General advice:
 
 1. The quality of your sleep may affect your general performance, 
 
-1.1. that is why getting at least 8 hours of sleep ensures you are sleeping enough, but not only that, you must be consistent with your sleep, sleeping this much as ofter as possible.
-2.2. that is why having an appropriate sleep environment is crucial for resting well. Try to keep it as similar to a blackout, and not eating nor using screens immediatly before going to sleep.
+1.1. that is why getting at least 8 hours of sleep ensures you are sleeping enough, but not only that, you must be consistent with your sleep, sleeping enough as often as possible.
+2.2. that is why having an appropriate sleep environment is crucial for resting well. Try to keep it as similar to a blackout, and not eating nor using screens immediately before going to sleep.
 
-2. Today's healthy tip is: Physical activity comes in a variety of ways, 
+2. Physical activity comes in a variety of ways, 
 
-2.1. a very interesting way is dancing, having fun with your friends in a party (watch out for alcohol!), learning new moves in an academy or just having a romantic night with your partner!
-2.2. like hiking, which gives you the oportunity to not only exercise but also contemplate beautiful scenes of nature while also giving yourself a dose of fresh air.
-2.3. being practicing soports a very traditional one. This one is particularly good for kids and young adults, as the discipline and team work that sports require provide with complementary skills.
-2.4. for example, you may find new ways to do daily activities in a more healthy way, like going to your school or job in a bycycle, avoiding taking escalators or elevators when possible.
+2.1. a very interesting way is dancing, having fun with your friends at a party (watch out for alcohol!), learning new moves in an academy or just having a romantic night with your partner!
+2.2. like hiking, which gives you the opportunity to not only exercise but also contemplate beautiful scenes of nature while also giving yourself a dose of fresh air.
+2.3. being practicing sports, a very traditional one. This one is particularly good for kids and young adults, as the discipline and team work that sports require provide with complementary skills.
+2.4. for example, you may find new ways to do daily activities in a more healthy way, like going to your school or job in a bicycle, avoiding taking escalators or elevators when possible.
 2.5. like, you're gonna love this one. Have sex with your partner, as if it wasn't fun on its own, it's cardio! And also strengths bonds within your relationship.
 
 
 3. Regarding nutrition, ask yourself, 
 
-3.1. what am I eating? Always try to eat balanced. Remember that you need carbs, fats, protein, vitamins, minerals and fiber, while keeping overprocessed and additives away.
-3.2. how am I eating? Steaming the ingredients that you don't want to eat raw is the best way to mantain a high amount of nutrients in them, or if you want to fry something, then use non-hydroginated oil and keep it clean.
-3.3. where am I eating? Eating in a place specifically designed for that helps digestion. The more comfortable you are and the less distractors around you the better you absorb your food.
+3.1. what am I eating? Always try to eat balanced. Remember that you need carbs, fats, protein, vitamins, minerals and fiber, while keeping over processed and additives away.
+3.2. how am I eating? Steaming the ingredients that you don't want to eat raw is the best way to maintain a high amount of nutrients in them, or if you want to fry something, then use non-hydrogenated oil and keep it clean.
+3.3. where am I eating? Eating in a place specifically designed for that helps digestion. The more comfortable you are and the fewer distractors around you, the better you absorb your food.
 
 4. Having support is the backup plan we all need, 
 
 4.1. it doesn't matter if it is a friend, la relative or your partner, we all need and will find very useful to have someone who pushes us further and give us a pat in the back whenever we are down.
-4.2. like being part of a social group or a tribe where you can find yourself among others of your kind, protects against depression or helps diminishing its effects.
+4.2. like being part of a social group or a tribe where you can find yourself among others of your kind, protects against depression or helps diminish its effects.
--- mixedMessages.js
+++ mixedMessages.js
@@ -1,14 +1,14 @@
 const baseSleep = "The quality of your sleep may affect your general performance, ";
 const baseActivity = "Physical activity comes in a variety of ways, ";
-const baseFood = "Regarding nutrition, ask yourself, ";
+const baseNutrition = "Regarding nutrition, ask yourself, ";
 const baseBackUp = "Having support is the backup plan we all need, ";
-const baseArr = [baseSleep, baseActivity, baseFood, baseBackUp];
+const baseArr = [baseSleep, baseActivity, baseNutrition, baseBackUp];
 
 
-const sleepArr = ["that is why getting at least 8 hours of sleep ensures you are sleeping enough, but not only that, you must be consistent with your sleep, sleeping this much as ofter as possible.", "that is why having an appropriate sleep environment is crucial for resting well. Try to keep it as similar to a blackout, and not eating nor using screens immediatly before going to sleep."];
-const activityArr = ["a very interesting way is dancing, having fun with your friends in a party (watch out for alcohol!), learning new moves in an academy or just having a romantic night with your partner!", "like hiking, which gives you the oportunity to not only exercise but also contemplate beautiful scenes of nature while also giving yourself a dose of fresh air.", "being practicing soports a very traditional one. This one is particularly good for kids and young adults, as the discipline and team work that sports require provide with complementary skills.", "for example, you may find new ways to do daily activities in a more healthy way, like going to your school or job in a bycycle, avoiding taking escalators or elevators when possible.", "like, you're gonna love this one. Have sex with your partner, as if it wasn't fun on its own, it's cardio! And also strengths bonds within your relationship."];
-const foodArr = ["what am I eating? Always try to eat balanced. Remember that you need carbs, fats, proteins, vitamins, minerals and fiber, while keeping overprocessed and additives away.", "how am I eating? Steaming the ingredients that you don't want to eat raw is the best way to mantain a high amount of nutrients in them, or if you want to fry something, then use non-hydroginated oil and keep it clean.", "where am I eating? Eating in a place specifically designed for that helps digestion. The more comfortable you are and the less distractors around you the better you absorb your food."];
-const backUpArr = ["it doesn't matter if it is a friend, a relative or your partner, we all need and will find very useful to have someone who pushes us further and give us a pat in the back whenever we are down.", "like being part of a social group or a tribe where you can find yourself among others of your kind, protects against depression or helps diminishing its effects."];
+const sleepArr = ["that is why getting at least 8 hours of sleep ensures you are sleeping enough, but not only that, you must be consistent with your sleep, sleeping enough as often as possible.", "that is why having an appropriate sleep environment is crucial for resting well. Try to keep it as similar to a blackout, and not eating nor using screens immediately before going to sleep."];
+const activityArr = ["a very interesting way is dancing, having fun with your friends at a party (watch out for alcohol!), learning new moves in an academy or just having a romantic night with your partner!", "like hiking, which gives you the opportunity to not only exercise but also contemplate beautiful scenes of nature while also giving yourself a dose of fresh air.", "being practicing sports, a very traditional one. This one is particularly good for kids and young adults, as the discipline and team work that sports require provide with complementary skills.", "for example, you may find new ways to do daily activities in a more healthy way, like going to your school or job in a bicycle, avoiding taking escalators or elevators when possible.", "like, you're gonna love this one. Have sex with your partner, as if it wasn't fun on its own, it's cardio! And also strengths bonds within your relationship."];
+const nutritionArr = ["what am I eating? Always try to eat balanced. Remember that you need carbs, fats, protein, vitamins, minerals and fiber, while keeping over processed and additives away.", "how am I eating? Steaming the ingredients that you don't want to eat raw is the best way to maintain a high amount of nutrients in them, or if you want to fry something, then use non-hydrogenated oil and keep it clean.", "where am I eating? Eating in a place specifically designed for that helps digestion. The more comfortable you are and the fewer distractors around you, the better you absorb your food."];
+const backUpArr = ["it doesn't matter if it is a friend, la relative or your partner, we all need and will find very useful to have someone who pushes us further and give us a pat in the back whenever we are down.", "like being part of a social group or a tribe where you can find yourself among others of your kind, protects against depression or helps diminish its effects."];
 
 const mixedMessage = () => {
     let baseSelect = baseArr[Math.floor(Math.random() * 4)];
@@ -23,8 +23,8 @@ const mixedMessage = () => {
             complementSelect = activityArr[Math.floor(Math.random() * 5)];
             printMessage = baseSelect + complementSelect;
             break;
-        case baseFood:
-            complementSelect = foodArr[Math.floor(Math.random() * 3)];
+        case baseNutrition:
+            complementSelect = nutritionArr[Math.floor(Math.random() * 3)];
             printMessage = baseSelect + complementSelect;
             break;
         case baseBackUp:
